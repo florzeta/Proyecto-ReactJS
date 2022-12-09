9e52bd09b1ae5e9b12f5de819e5a67739c6c4352
florzeta <99660646+florzeta@users.noreply.github.com>
2022-12-09 12:34:14 -0300

    arreglos

--- myshop/src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ myshop/src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 import { consultarBDD } from "../../assets/funciones";
 import ItemDetail from "../ItemDetail/ItemDetail";
 const ItemDetailContainer = () => {
     const [producto, setProducto] = useState([]);
-
+    const{id} = useParams()
     useEffect(() => {
-        consultarBDD().then(productos => {
-            const prod = productos.find(product => product.id === 2)
+        consultarBDD('../json/producto.json').then(productos => {
+            const prod = productos.find(product => product.id === parseInt(id))
             setProducto(prod)
         })
 
