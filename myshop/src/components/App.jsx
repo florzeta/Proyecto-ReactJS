c6d8ab0c254da254b3add729ded7e8710716a6ea
florzeta <99660646+florzeta@users.noreply.github.com>
2022-12-20 22:58:29 -0300

    arreglos filtrado de categorias

--- myshop/src/components/App.jsx
+++ myshop/src/components/App.jsx
@@ -16,11 +16,10 @@ const App = () => {
         <Routes>
           <Route path='/' element={<ItemListContainer />}/>
           <Route path='/producto/:id' element={<ItemDetailContainer/>}/>
-          <Route path='/category/:id' element={<ItemListContainer/>}/>
+          <Route path='/category/:category' element={<ItemListContainer/>}/>
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/checkout' element={<Checkout/>}/>
         </Routes>
-        
       </BrowserRouter>
     </>
   );
--- myshop/src/components/ItemListContainer/ItemListContainer.jsx
+++ myshop/src/components/ItemListContainer/ItemListContainer.jsx
@@ -4,32 +4,32 @@ import ItemList from '../ItemList/ItemList';
 import { consultarBDD } from '../../assets/funciones.js'
 const ItemListContainer = () => {
     const [productos, setProductos] = useState([]);
-    const {category} = useParams()
-    
+    const { category } = useParams()
+
     useEffect(() => {
-        if(category) {
+        if (category) {
             consultarBDD('../json/productos.json').then(products => {
-                const productsList= products.filter(prod => prod.stock > 0).filter(prod => prod.idCategoria === parseInt(category))
+                const productsList = products.filter(prod => prod.stock > 0).filter(prod => prod.idCategoria === parseInt(category))
                 const cardProductos = ItemList({productsList})
                 setProductos(cardProductos)
             })
         } else {
             consultarBDD('./json/productos.json').then(products => {
-                const productsList= products.filter(prod => prod.stock > 0)
+                const productsList = products.filter(prod => prod.stock > 0)
                 const cardProductos = ItemList({productsList})
                 setProductos(cardProductos)
             })
         }
-        
-    
-},[category]);
 
-return (
-    <div className= 'row cardProductos' >
-        {productos}
-    </div>
-   
-);
+
+    },[category]);
+
+    return (
+        <div className='row cardProductos' >
+            {productos}
+        </div>
+
+    );
 }
 
 export default ItemListContainer;
